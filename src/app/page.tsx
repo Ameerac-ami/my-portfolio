8dfb9696dbacfe47781aa95fcffc05b029c8c74a
Ameerac-ami <ameerac.ami@gmail.com>
2026-03-23 09:40:18 +0530

    Experience section added to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import AboutSection from "@/components/sections/AboutSection";
 import SkillsSection from "@/components/sections/SkillsSection";
 import ProjectsSection from "@/components/sections/ProjectsSection";
 import ContactSection from "@/components/sections/ContactSection";
+import ExperienceSection from "@/components/sections/ExperienceSection";
 
 export default function Home() {
     useReveal();
@@ -90,6 +91,7 @@ export default function Home() {
             <AboutSection />
             <SkillsSection />
             <ProjectsSection />
+            <ExperienceSection />
             <ContactSection />
         </div>
     );
