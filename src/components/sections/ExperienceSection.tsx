874bcb06fb6a5737e6817d457058675ded16c18d
Ameerac-ami <ameerac.ami@gmail.com>
2026-03-27 13:17:38 +0530

    Color syle changed in exerience section

--- src/components/sections/ExperienceSection.tsx
+++ src/components/sections/ExperienceSection.tsx
@@ -118,7 +118,7 @@ export default function ExperienceSection() {
                                         >
                                             <span className="relative flex h-2 w-2">
                                                 {/* <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-accent opacity-75"></span> */}
-                                                <span className="relative inline-flex h-2 w-2 rounded-full bg-accent"></span>
+                                                <span className="relative inline-flex h-2 w-2 rounded-full bg-slate-400"></span>
                                             </span>
                                             <span className="text-xs font-medium text-slate-600 tracking-wider">
                                                 {exp.type}
